19a1543ed099b57bf9ffd0b5ee9423264000f667
Your Name <Your Email>
2020-12-24 01:37:28 +0100

    Day06 Part2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,11 +1,32 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn count_yes(group: &String) -> usize {
-    // println!("{}", group);
+    // remove doublons
     let set : HashSet<char> = group.chars().collect();
-    set.len()
+    // need to remove '\n'
+    set.len() - 1
+}
+
+fn count_all_yes(group: &String) -> usize {
+    // We count elements that are present the "number of line" times.
+    let mut sets = HashMap::new();
+    let mut peoplenb = 0;
+    let mut count = 0;
+    for line in group.lines() {
+        peoplenb += 1;
+        for ch in line.chars() {
+            let counter = sets.entry(ch).or_insert(0);
+            *counter += 1; 
+        }
+    }
+    for (_,val) in sets.iter() {
+        if val == &peoplenb {
+            count += 1;
+        }
+    }
+    count
 }
 
 fn get_groups(input: &String) -> Vec<String> {
@@ -13,28 +34,11 @@ fn get_groups(input: &String) -> Vec<String> {
     let mut group = String::new();
     let file = File::open(input).expect("Error opening file");
 
-    //let mut lines = Vec::new();
-    //lines.push("abc");
-    //lines.push("");
-    //lines.push("a");
-    //lines.push("b");
-    //lines.push("c");
-    //lines.push("");
-    //lines.push("ab");
-    //lines.push("ac");
-    //lines.push("");
-    //lines.push("a");
-    //lines.push("a");
-    //lines.push("a");
-    //lines.push("a");
-    //lines.push("");
-    //lines.push("b");
-
     for line in BufReader::new(file).lines() {
-    //for line in lines {
-        let line = line.expect("Bufreader.lines error");
+        let line = line.expect("Bufreader.lines error") ;
         if line.len() > 0 {
-            group.push_str(&line);
+            // BufReader removes the terminal \n
+            group.push_str(&(line+ "\n"));
         }
         else {
             groups.push(group);
@@ -50,10 +54,16 @@ fn main() -> Result<(), std::io::Error> {
     let groups = get_groups(&"input".to_string());
     println!("group nb: {}", groups.len());
     let mut sum_yes = 0;
-    for group in groups {
+    for group in &groups {
         sum_yes += count_yes(&group);
     }
     println!("Part1: {}", sum_yes);
+
+    let mut sum_yes = 0;
+    for group in &groups {
+        sum_yes += count_all_yes(&group);
+    }
+    println!("Part2: {}", sum_yes);
 
     Ok(())
 }
